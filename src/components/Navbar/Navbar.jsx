6e09777f442b945eb972bfe2b073444c4405d60e
benedictusron <benedictusron99@gmail.com>
2025-11-09 11:46:45 +0700

    fix: restyling responsive header & restructuring code on header & profile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Logo from '../../assets/Logo.png';
 import Profile from '../Navbar/Profile';
 import ListMenuProfile from './ListMenuProfile';
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
-  return (
-    <header className='bg-light-primary shadow-sm'>
-        <nav className="container mx-auto max-w-[1200px] px-4 md:px-6 lg:px-10 py-4 md:flex md:justify-between md:items-center ">
-        {/* <nav className="p-5 bg-light-primary  border-b border-other-border md:flex md:items-center md:justify-between sm:px-[120px]"> */}
-            <div className="flex justify-between items-center">
-                <div className="inline">
-                    <img src={Logo} alt="Logo" className="rounded-[10px] object-cover w-48 h-[30px]"/>
-                </div>
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 768) {
+                setIsOpen(false);
+            }
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [])
 
-                <button className="text-3xl cursor-pointer md:hidden block mx-2" onClick={() => setIsOpen(!isOpen)}>
-                    <svg width="20" height="13" viewBox="0 0 20 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 10.5V12.5H20V10.5H0ZM0 5.5V7.5H20V5.5H0ZM0 0.5V2.5H20V0.5H0Z" fill="#4A505C" />
-                    </svg>
-                </button>
-
-                {isOpen && (
-                      <ListMenuProfile/>
-                )}
+    return (
+        <header className='bg-light-primary shadow-sm'>
+            <div className='flex justify-between items-center container mx-auto max-w-[1200px] px-4 md:px-6 lg:px-10   py-4 md:flex md:justify-between md:items-center' >
+                    <div className="inline">
+                        <img src={Logo} alt="Logo" className="rounded-[10px] object-coverh-[30px]" />
+                    </div>
+                <Profile isOpen={isOpen} setIsOpen={setIsOpen} />
             </div>
+        </header >
 
-            <ul className="md:flex md:items-center md:justify-between
-                       z-[-1] md:z-auto md:static absolute bg-light-primary w-full left-0  md:w-auto md:py-0
-                       py-4 md:pl-0 pl-5 md:opacity-100 opacity-0 top-[400px]">
-                <li className="mx-4 my-6 md:my-0 ">
-                    <a href="" className='text-base text-black-secondary'>Kategori</a>
-                </li>
-                <Profile/>
-            </ul>
-        </nav>
-    </header>
-  )
+    );
+
+
 }
 
 export default Navbar
--- src/components/Navbar/Profile.jsx
+++ src/components/Navbar/Profile.jsx
@@ -3,8 +3,8 @@ import Ava from '../../assets/Avatar.png';
 import ListMenuProfile from './ListMenuProfile';
 
 function Profile() {
-    const [isOpen, setIsOpen] = useState(false);
-    const dropdownRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -14,22 +14,37 @@ function Profile() {
     };
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
-    return (
-        <div ref={dropdownRef} className='relative'>
-            <button onClick={() => setIsOpen(!isOpen)}
-                className="flex items-center focus:outline-none">
-                <img
-                    src={Ava}
-                    alt="Profile Ava"
-                    className='' />
-            </button>
+  }, [setIsOpen]);
 
-            {isOpen && (
-                <ListMenuProfile />
-            )}
+  return (
+    <div ref={dropdownRef} className='relative'>
+      <button onClick={() => setIsOpen(!isOpen)}
+        className="flex items-center focus:outline-none">
+
+        <span className='text-3xl md:hidden  mx-2'>
+          <svg width="20" height="13" viewBox="0 0 20 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 10.5V12.5H20V10.5H0ZM0 5.5V7.5H20V5.5H0ZM0 0.5V2.5H20V0.5H0Z" fill="#4A505C" />
+          </svg>
+        </span>
+      </button>
+
+      <div className='hidden md:block'>
+        <div className='flex justify-between items-center gap-9'>
+          <nav>
+            <a href="" className="text-base text-black-secondary">Kategori</a>
+          </nav>
+          <img
+            src={Ava}
+            alt="Profile Ava"
+            className='' onClick={() => setIsOpen(!isOpen)} />
         </div>
-    )
+      </div>
+
+      {isOpen && (
+        <ListMenuProfile />
+      )}
+    </div>
+  )
 }
 
 export default Profile
